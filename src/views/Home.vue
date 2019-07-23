dd114b74520cee6bdae2d3de2d2ced97817a0e3a
Pham Ba Thanh <Thanhh181998@gmail.com>
2019-07-23 23:46:54 +0700

    First Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="d-flex justify-content-center">
+   <div class="home">
+
+    
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Password: " label-for="input-2">
+        <b-form-input 
+          type="password"
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter you password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          name: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+      }
+    }
   }
-}
 </script>
+
+<style>
+
+.home {
+  border: solid 1px black;
+  border-radius: 10px;
+  width: 30%;
+  padding: 5px;
+}
+
+</style>
