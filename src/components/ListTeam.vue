00b50e207c3f43b0eeb81bee3e9b32318f4b8a3f
Pham Ba Thanh <Thanhh181998@gmail.com>
2019-07-25 11:31:26 +0700

    Done reddirect

--- src/components/ListTeam.vue
+++ src/components/ListTeam.vue
@@ -1,56 +1,60 @@
 <template>
-    <div>
-        <router-link :to="{ name: 'listTeam' }" tag="button" class="btn btn-default glyphicon glyphicon-plus">List Team</router-link>
-        <router-link :to="{ name: 'listPlayer' }" tag="button" class="btn btn-default glyphicon glyphicon-plus">List Player</router-link>
-        <router-link :to="{ name: 'listManager' }" tag="button" class="btn btn-default glyphicon glyphicon-plus">List Manager</router-link>
-        <hr/>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>
-                        ID
-                    </th>
-                    <th>
-                        Team
-                    </th>
-                    <th>
-                        City
-                    </th>
-                    <th>
-                        Nation
-                    </th>
-                </tr>
-            </thead>
-            <tbody v-for="(item, index) in items" v-bind:key="item">
-                <tr>
-                    <td>{{ index+1 }}</td>
-                    <td>{{ item.team }}</td>
-                    <td>{{ item.city }}</td>
-                    <td>{{ item.nation }}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+  <div>
+    <router-link
+      :to="{ name: 'listTeam' }"
+      tag="button"
+      class="btn btn-success glyphicon glyphicon-plus"
+    >List Team</router-link>
+    <router-link
+      :to="{ name: 'listPlayer' }"
+      tag="button"
+      class="btn btn-success glyphicon glyphicon-plus"
+    >List Player</router-link>
+    <router-link
+      :to="{ name: 'listManager' }"
+      tag="button"
+      class="btn btn-success glyphicon glyphicon-plus"
+    >List Manager</router-link>
+    <hr />
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Team</th>
+          <th>City</th>
+          <th>Nation</th>
+        </tr>
+      </thead>
+      <tbody v-for="(item, index) in items" v-bind:key="item">
+        <tr>
+          <td>{{ index+1 }}</td>
+          <td>{{ item.team }}</td>
+          <td>{{ item.city }}</td>
+          <td>{{ item.nation }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
-    export default {
-        name : 'List',
-        props: ['data'],
-        data() {
-            return {
-                items: [
-                { 'team': 'Arsenal', 'city': 'London', 'nation': 'England' },
-                { 'team': 'Barcelona', 'city': 'Catalunya', 'nation': 'Spain' },
-                { 'team': 'Real Madrid', 'city': 'Madrid', 'nation': 'Spain' },
-                { 'team': 'Paris Saint Germain', 'city': 'Paris', 'nation': 'Franch' },
-                { 'team': 'Manchester City', 'city': 'Manchester', 'nation': 'England' },
-                ]
-            }
-        }
+export default {
+  name: 'List',
+  props: ['data'],
+  data () {
+    return {
+      items: [
+        { team: 'Arsenal', city: 'London', nation: 'England' },
+        { team: 'Barcelona', city: 'Catalunya', nation: 'Spain' },
+        { team: 'Real Madrid', city: 'Madrid', nation: 'Spain' },
+        { team: 'Paris Saint Germain', city: 'Paris', nation: 'Franch' },
+        { team: 'Manchester City', city: 'Manchester', nation: 'England' }
+      ]
     }
+  }
+}
 </script>
 <style type="text/css">
-    tr > th {
-        text-align: center !important;
-    }
+tr > th {
+  text-align: center !important;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,79 +1,77 @@
 <template>
   <div class="d-flex justify-content-center">
-   <div class="home">
-
-   <!-- <div class="message">
+    <div class="home">
+      <!-- <div class="message">
      <p v-if="loginSuccess"> Login successfull </p>
      <p v-else> The username and password is not exist </p>
-   </div> -->
+      </div>-->
 
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
 
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group id="input-group-2" label="Password: " label-for="input-2">
+          <b-form-input
+            type="password"
+            id="input-2"
+            v-model="form.password"
+            required
+            placeholder="Enter you password"
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-group-2" label="Password: " label-for="input-2">
-        <b-form-input 
-          type="password"
-          id="input-2"
-          v-model="form.password"
-          required
-          placeholder="Enter you password"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          password: '',
-          loginSuccess: false
-        },
-        show: true
+import router from '@/router'
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: '',
+        loginSuccess: false
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      if (
+        this.form.email !== 'admin@fsoft.com.vn' ||
+        this.form.password !== '12345678'
+      ) {
+        alert('The username or password not exist!')
+      } else {
+        //alert('Login successfull!')
+        // router.push('/team/list')
+        router.push('/team/list')
       }
     },
-    methods: {
-      onSubmit: function(evt) {
-        evt.preventDefault()
-        if (this.form.email !== 'admin@fsoft.com.vn' || this.form.password !== '12345678') {
-          alert("The user or password not exist!");
-        } else {
-          alert("Login successfull!");
-          
-        }
-      },
-      onReset: function(evt) {
-        evt.preventDefault()
-        // Reset our form values
-        this.form.email = ''
-        this.form.password = ''
-      }
+    onReset (evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.password = ''
     }
   }
+}
 </script>
 
 <style>
-
 .home {
   border: solid 1px black;
   border-radius: 10px;
@@ -81,5 +79,4 @@
   padding: 5px;
   padding-top: 10px;
 }
-
 </style>
